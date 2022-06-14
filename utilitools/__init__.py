065e0069addf44fe1216dd98b2206defeedfd7ac
idan <idan.hazan.88@gmail.com>
2022-06-14 23:52:39 +0300

    Update __init__.py

--- utilitools/__init__.py
+++ utilitools/__init__.py
@@ -1,4 +1,5 @@
-from .singleton import Singleton
+from .partial import Partial as partial
+from .singleton import Singleton as singleton
 from .subscriptable import subscriptable
 
 __version__ = (0, 0, 6)
