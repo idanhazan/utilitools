da41b0691987d3b70d7c1220e6951a6424774564
idan <idan.hazan.88@gmail.com>
2022-06-22 19:18:04 +0300

    Update test_singleton.py

--- utilitools/tests/test_singleton.py
+++ utilitools/tests/test_singleton.py
@@ -1,4 +1,3 @@
-import typing
 import unittest
 import utilitools
 
@@ -8,21 +7,10 @@ class Singleton(metaclass=utilitools.singleton):
 
 
 class Test(unittest.TestCase):
-    instance_x: typing.Any
-    instance_y: typing.Any
-
-    @classmethod
-    def setUpClass(cls):
-        cls.instance_x = Singleton()
-        cls.instance_y = Singleton()
-
-    @classmethod
-    def tearDownClass(cls):
-        del cls.instance_x
-        del cls.instance_y
-
     def test_same_object(self):
-        self.assertIs(self.instance_x, self.instance_y)
+        instance_x = Singleton()
+        instance_y = Singleton()
+        self.assertIs(instance_x, instance_y)
 
 
 if __name__ == '__main__':
