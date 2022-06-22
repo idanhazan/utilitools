f7f452e51de0111749130f7d8046cdc8d7bd4c86
idan <idan.hazan.88@gmail.com>
2022-06-22 19:17:21 +0300

    Update test_partial.py

--- utilitools/tests/test_partial.py
+++ utilitools/tests/test_partial.py
@@ -1,42 +1,30 @@
-import typing
 import unittest
 import utilitools
 
 
 class Test(unittest.TestCase):
-    isinstance_int: typing.Any
-    isinstance_str: typing.Any
+    def test_variadic_function(self):
+        func = utilitools.partial(lambda *args, **kwargs: (args, kwargs), ..., 2, ..., d=4)
+        positionals, keywords = (1, 2, 3), {'d': 4, 'e': 5}
+        self.assertEqual(func(1, 3, e=5), (positionals, keywords))
+        positionals, keywords = (1, 2), {'d': 4, 'c': 3, 'e': 5}
+        self.assertEqual(func(1, c=3, e=5), (positionals, keywords))
 
-    @classmethod
-    def setUpClass(cls):
-        cls.isinstance_int = utilitools.partial(isinstance, ..., int)
-        cls.isinstance_str = utilitools.partial(isinstance, ..., str)
+    def test_positional_only(self):
+        func = utilitools.partial(lambda a, b, c, /: (a, b, c), ..., 2, ...)
+        self.assertEqual(func(1, 3), (1, 2, 3))
 
-    @classmethod
-    def tearDownClass(cls):
-        del cls.isinstance_int
-        del cls.isinstance_str
+    def test_positional_or_keyword(self):
+        func = utilitools.partial(lambda a, b, /, c, d, *, e, f: (a, b, c, d, e, f), ..., 2, ..., 4, f=6)
+        self.assertEqual(func(1, 3, e=5), (1, 2, 3, 4, 5, 6))
+        self.assertEqual(func(1, c=3, e=5), (1, 2, 3, 4, 5, 6))
+        func = utilitools.partial(lambda a, b, /, c, d, *, e, f: (a, b, c, d, e, f), ..., 2, ..., d=4, f=6)
+        self.assertEqual(func(1, 3, e=5), (1, 2, 3, 4, 5, 6))
+        self.assertEqual(func(1, c=3, e=5), (1, 2, 3, 4, 5, 6))
 
-    def test_isinstance(self):
-        self.assertEqual(all(map(self.isinstance_int, [1, 2])), True)
-        self.assertEqual(all(map(self.isinstance_int, ['1', 2])), False)
-        self.assertEqual(all(map(self.isinstance_int, [1, '2'])), False)
-        self.assertEqual(all(map(self.isinstance_int, ['1', '2'])), False)
-
-        self.assertEqual(all(map(self.isinstance_str, [1, 2])), False)
-        self.assertEqual(all(map(self.isinstance_str, ['1', 2])), False)
-        self.assertEqual(all(map(self.isinstance_str, [1, '2'])), False)
-        self.assertEqual(all(map(self.isinstance_str, ['1', '2'])), True)
-
-        self.assertEqual(any(map(self.isinstance_int, [1, 2])), True)
-        self.assertEqual(any(map(self.isinstance_int, ['1', 2])), True)
-        self.assertEqual(any(map(self.isinstance_int, [1, '2'])), True)
-        self.assertEqual(any(map(self.isinstance_int, ['1', '2'])), False)
-
-        self.assertEqual(any(map(self.isinstance_str, [1, 2])), False)
-        self.assertEqual(any(map(self.isinstance_str, ['1', 2])), True)
-        self.assertEqual(any(map(self.isinstance_str, [1, '2'])), True)
-        self.assertEqual(any(map(self.isinstance_str, ['1', '2'])), True)
+    def test_keyword_only(self):
+        func = utilitools.partial(lambda *, a, b, c: (a, b, c), b=2)
+        self.assertEqual(func(a=1, c=3), (1, 2, 3))
 
 
 if __name__ == '__main__':
