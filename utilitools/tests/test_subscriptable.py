5b130389048fada524922238d178a46ae03c3070
idan <idan.hazan.88@gmail.com>
2022-06-25 03:40:08 +0300

    Update test_subscriptable.py

--- utilitools/tests/test_subscriptable.py
+++ utilitools/tests/test_subscriptable.py
@@ -16,23 +16,45 @@ def generator():
 class Test(unittest.TestCase):
     def test_function_by_index(self):
         index = 75
-        self.assertEqual(function[index], index)
+        actual = function[index]
+        expected = index
+        self.assertEqual(expected, actual)
+        actual = function[-index]
+        expected = -index
+        self.assertEqual(expected, actual)
 
     def test_function_by_slice(self):
         start, stop, end = 10, 25, 3
-        self.assertEqual(function[:stop], tuple(range(stop)))
-        self.assertEqual(function[start:stop], tuple(range(start, stop)))
-        self.assertEqual(function[start:stop:end], tuple(range(start, stop, end)))
+        actual = function[:stop]
+        expected = tuple(range(stop))
+        self.assertEqual(expected, actual)
+        actual = function[start:stop]
+        expected = tuple(range(start, stop))
+        self.assertEqual(expected, actual)
+        actual = function[start:stop:end]
+        expected = tuple(range(start, stop, end))
+        self.assertEqual(expected, actual)
+        actual = function[stop:start:-end]
+        expected = tuple(range(stop, start, -end))
+        self.assertEqual(expected, actual)
 
     def test_generator_by_index(self):
         index = 75
-        self.assertEqual(generator[index], index)
+        actual = generator[index]
+        expected = index
+        self.assertEqual(expected, actual)
 
     def test_generator_by_slice(self):
         start, stop, end = 10, 25, 3
-        self.assertEqual(generator[:stop], list(range(stop)))
-        self.assertEqual(generator[start:stop], list(range(start, stop)))
-        self.assertEqual(generator[start:stop:end], list(range(start, stop, end)))
+        actual = generator[:stop]
+        expected = list(range(stop))
+        self.assertEqual(expected, actual)
+        actual = generator[start:stop]
+        expected = list(range(start, stop))
+        self.assertEqual(expected, actual)
+        actual = generator[start:stop:end]
+        expected = list(range(start, stop, end))
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
