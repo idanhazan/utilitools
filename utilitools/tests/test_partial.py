baa4d0e2873c41b1658cdeb4f93c3a2b2fb18177
idan <idan.hazan.88@gmail.com>
2022-06-25 03:39:06 +0300

    Update test_partial.py

--- utilitools/tests/test_partial.py
+++ utilitools/tests/test_partial.py
@@ -2,29 +2,60 @@ import unittest
 import utilitools
 
 
+def variadic_positional(*args):
+    return args
+
+
+def variadic_keyword(**kwargs):
+    return kwargs
+
+
+def positional_only(a, b, c, d=4, e=5, f=6, /):
+    return a, b, c, d, e, f
+
+
+def positional_or_keyword(a, b, c, d=4, e=5, f=6):
+    return a, b, c, d, e, f
+
+
+def keyword_only(*, a, b, c, d=4, e=5, f=6):
+    return a, b, c, d, e, f
+
+
 class Test(unittest.TestCase):
-    def test_variadic_function(self):
-        func = utilitools.partial(lambda *args, **kwargs: (args, kwargs), ..., 2, ..., d=4)
-        positionals, keywords = (1, 2, 3), {'d': 4, 'e': 5}
-        self.assertEqual(func(1, 3, e=5), (positionals, keywords))
-        positionals, keywords = (1, 2), {'d': 4, 'c': 3, 'e': 5}
-        self.assertEqual(func(1, c=3, e=5), (positionals, keywords))
+    def test_variadic_positional(self):
+        partial = utilitools.partial(variadic_positional, ..., ..., 3, 4)
+        actual = partial(1, 2, 5, 6)
+        expected = (1, 2, 3, 4, 5, 6)
+        self.assertEqual(expected, actual)
+
+    def test_variadic_keyword(self):
+        partial = utilitools.partial(variadic_keyword)
+        actual = partial(a=1, b=2, c=3, d=4, e=5, f=6)
+        expected = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+        self.assertEqual(expected, actual)
 
     def test_positional_only(self):
-        func = utilitools.partial(lambda a, b, c, /: (a, b, c), ..., 2, ...)
-        self.assertEqual(func(1, 3), (1, 2, 3))
+        partial = utilitools.partial(positional_only, ..., ..., 3, 4)
+        actual = partial(1, 2, 5, 6)
+        expected = (1, 2, 3, 4, 5, 6)
+        self.assertEqual(expected, actual)
 
     def test_positional_or_keyword(self):
-        func = utilitools.partial(lambda a, b, /, c, d, *, e, f: (a, b, c, d, e, f), ..., 2, ..., 4, f=6)
-        self.assertEqual(func(1, 3, e=5), (1, 2, 3, 4, 5, 6))
-        self.assertEqual(func(1, c=3, e=5), (1, 2, 3, 4, 5, 6))
-        func = utilitools.partial(lambda a, b, /, c, d, *, e, f: (a, b, c, d, e, f), ..., 2, ..., d=4, f=6)
-        self.assertEqual(func(1, 3, e=5), (1, 2, 3, 4, 5, 6))
-        self.assertEqual(func(1, c=3, e=5), (1, 2, 3, 4, 5, 6))
+        partial = utilitools.partial(positional_or_keyword, ..., 2, ..., ..., 5, f=6)
+        actual = partial(1, 3, 4)
+        expected = (1, 2, 3, 4, 5, 6)
+        self.assertEqual(expected, actual)
+        partial = utilitools.partial(positional_or_keyword, ..., 2, ..., e=5, f=6)
+        actual = partial(1, 3, d=4)
+        expected = (1, 2, 3, 4, 5, 6)
+        self.assertEqual(expected, actual)
 
     def test_keyword_only(self):
-        func = utilitools.partial(lambda *, a, b, c: (a, b, c), b=2)
-        self.assertEqual(func(a=1, c=3), (1, 2, 3))
+        partial = utilitools.partial(keyword_only, b=2, d=4, f=6)
+        actual = partial(a=1, c=3, e=5)
+        expected = (1, 2, 3, 4, 5, 6)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
