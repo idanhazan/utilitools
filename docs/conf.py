de2cdcc0bedb0eb855371dccd3e42be2793931a4
idan <idan.hazan.88@gmail.com>
2022-06-25 14:35:58 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,6 @@ import utilitools
 project = 'utilitools'
 author = 'Idan Hazan'
 copyright = f'2022, {author}'
-version = utilitools.__version__
 html_theme = 'sphinx_rtd_theme'
 extensions = [
     'sphinx.ext.autodoc',
