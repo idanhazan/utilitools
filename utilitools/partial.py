887be3432826da31d8f27dc3c519fa4f6324764d
idan <idan.hazan.88@gmail.com>
2022-06-25 18:10:15 +0300

    Update partial.py

--- utilitools/partial.py
+++ utilitools/partial.py
@@ -5,7 +5,7 @@ import itertools
 class Partial(functools.partial):
     def __call__(self, *args, **kwargs):
         iterator = iter(args + tuple(itertools.repeat(..., self.args.count(...) - len(args))))
-        positionals = tuple(next(iterator) if arg is ... else arg for arg in self.args) + tuple(iterator)
+        positionals = tuple(next(iterator) if arg is ... else arg for arg in self.args)
+        positionals = tuple(filter(lambda arg: arg is not ..., positionals + tuple(iterator)))
         keywords = dict(**self.keywords, **kwargs)
-        positionals = tuple(filter(lambda arg: arg is not ..., positionals))
         return self.func(*positionals, **keywords)
