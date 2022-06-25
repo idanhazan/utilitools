98e4dd8e3f34b7a1fbc4fd2dc01a3982d67cd4b4
idan <idan.hazan.88@gmail.com>
2022-06-25 14:31:07 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,6 @@ copyright = f'2022, {author}'
 version = utilitools.__version__
 html_theme = 'sphinx_rtd_theme'
 extensions = [
-    'readthedocs_ext.readthedocs',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx_toolbox.more_autodoc',
     'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
 ]
