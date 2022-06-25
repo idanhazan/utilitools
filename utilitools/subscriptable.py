7144513c1ffff54faab64ef70c00f75d2ea41f14
idan <idan.hazan.88@gmail.com>
2022-06-25 13:13:03 +0300

    Update subscriptable.py

--- utilitools/subscriptable.py
+++ utilitools/subscriptable.py
@@ -3,12 +3,13 @@ import inspect
 import itertools
 import math
 import sys
+import typing
 
 
-class Sequence:
-    def __init__(self, func, dtype):
+class Subscription:
+    def __init__(self, func, sliced_type):
         self._func = func
-        self._dtype = dtype
+        self._type = sliced_type
 
     def __getitem__(self, key):
         if isinstance(key, int):
@@ -31,10 +32,43 @@ class Sequence:
             else:
                 iterable = map(self._func, itertools.count(key.start, key.step))
                 iterator = itertools.islice(iterable, math.ceil((key.stop - key.start) / key.step))
-            return self._dtype(iterator) if self._dtype else iterator
+            return self._type(iterator) if self._type is not None else iterator
 
 
-def subscriptable(dtype=None, /):
+def subscriptable(sliced_type: typing.Any = None, /) -> typing.Callable[[typing.Any], Subscription]:
+    """
+    A decorator that transforms a function into a subscription object.
+
+        >>> from utilitools import subscriptable
+
+        >>> @subscriptable(tuple)
+        >>> def digital_sum(n):
+        >>>    return sum(map(int, str(n)))
+
+        >>> @subscriptable(list)
+        >>> def fibonacci():
+        >>>     a, b = 0, 1
+        >>>     yield a
+        >>>     while True:
+        >>>        a, b = b, a + b
+        >>>         yield a
+
+        >>> digital_sum[123]
+        6
+        >>> digital_sum[10:20:3]
+        (1, 4, 7, 10)
+        >>> fibonacci[123]
+        22698374052006863956975682
+        >>> fibonacci[10:20:3]
+        [55, 233, 987, 4181]
+
+    :param sliced_type:
+        Slicing can yield more than one value,
+        and you can choose the return type by passing `list` or `tuple` or any else.
+        By default, return an `itertools.islice` object.
+    :return:
+        A subscription object.
+    """
     def wrapper(func):
-        return Sequence(func, dtype)
+        return Subscription(func, sliced_type)
     return wrapper
