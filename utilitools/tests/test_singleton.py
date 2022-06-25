fdb429562c5aa00439e12d6dc7e1c87531237a87
idan <idan.hazan.88@gmail.com>
2022-06-25 03:39:39 +0300

    Update test_singleton.py

--- utilitools/tests/test_singleton.py
+++ utilitools/tests/test_singleton.py
@@ -7,10 +7,10 @@ class Singleton(metaclass=utilitools.singleton):
 
 
 class Test(unittest.TestCase):
-    def test_same_object(self):
-        instance_x = Singleton()
-        instance_y = Singleton()
-        self.assertIs(instance_x, instance_y)
+    def test_singleton(self):
+        actual = Singleton()
+        expected = Singleton()
+        self.assertIs(expected, actual)
 
 
 if __name__ == '__main__':
