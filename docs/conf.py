3826c9e8a11bdc47915c16d75786bfca406905de
idan <idan.hazan.88@gmail.com>
2022-06-25 14:27:12 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,3 +10,10 @@ author = 'Idan Hazan'
 copyright = f'2022, {author}'
 version = utilitools.__version__
 html_theme = 'sphinx_rtd_theme'
+extensions = [
+    'readthedocs_ext.readthedocs',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx_toolbox.more_autodoc',
+    'sphinx.ext.autodoc',
+]
