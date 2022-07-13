1a2a23ac0abe6170fc6fb49c8b7503be1a398d9e
idan <idan.hazan.88@gmail.com>
2022-07-13 17:43:16 +0300

    Update test_subscriptable.py

--- utilitools/tests/test_subscriptable.py
+++ utilitools/tests/test_subscriptable.py
@@ -24,18 +24,18 @@ class Test(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_function_by_slice(self):
-        start, stop, end = 10, 25, 3
+        start, stop, step = 10, 25, 3
         actual = function[:stop]
         expected = tuple(range(stop))
         self.assertEqual(expected, actual)
         actual = function[start:stop]
         expected = tuple(range(start, stop))
         self.assertEqual(expected, actual)
-        actual = function[start:stop:end]
-        expected = tuple(range(start, stop, end))
+        actual = function[start:stop:step]
+        expected = tuple(range(start, stop, step))
         self.assertEqual(expected, actual)
-        actual = function[stop:start:-end]
-        expected = tuple(range(stop, start, -end))
+        actual = function[stop:start:-step]
+        expected = tuple(range(stop, start, -step))
         self.assertEqual(expected, actual)
 
     def test_generator_by_index(self):
@@ -45,15 +45,15 @@ class Test(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_generator_by_slice(self):
-        start, stop, end = 10, 25, 3
+        start, stop, step = 10, 25, 3
         actual = generator[:stop]
         expected = list(range(stop))
         self.assertEqual(expected, actual)
         actual = generator[start:stop]
         expected = list(range(start, stop))
         self.assertEqual(expected, actual)
-        actual = generator[start:stop:end]
-        expected = list(range(start, stop, end))
+        actual = generator[start:stop:step]
+        expected = list(range(start, stop, step))
         self.assertEqual(expected, actual)
 
 
