64fd54cc3e78f98784b87ba673502f79d8a9c053
idan <idan.hazan.88@gmail.com>
2022-07-14 12:56:47 +0300

    Update islice.py

--- utilitools/islice.py
+++ utilitools/islice.py
@@ -34,23 +34,19 @@ def islice(iterable, *args):
     key = slice(*args)
     step = 1 if key.step is None else key.step
     length = max(getattr(iterable, '__len__', lambda: -1)(), getattr(iterable, '__length_hint__', lambda: -1)())
-    if step == 0:
-        raise ValueError('slice step cannot be zero')
-    elif length == 0:
-        return
-    elif step > 0 and length > 0:
+    if step >= 0 and length >= 0:
         start = 0 if key.start is None else key.start
         stop = length if key.stop is None else key.stop
         start = length + start if start < 0 else start
         stop = length + stop if stop < 0 else stop
         yield from itertools.islice(iterable, start, stop, step)
-    elif step < 0 and length > 0 and hasattr(iterable, '__reversed__'):
+    elif step < 0 and length >= 0 and hasattr(iterable, '__reversed__'):
         start = -1 if key.start is None else key.start
         stop = -length if key.stop is None else key.stop
         start = length - (length + start) - 1 if start < 0 else length - start - 1
         stop = length - (length + stop) - 1 if stop < 0 else length - stop - 1
         yield from itertools.islice(reversed(iterable), start, stop, -step)
-    elif step > 0:
+    elif step >= 0:
         start = 0 if key.start is None else key.start
         stop = sys.maxsize if key.stop is None else key.stop
         if start >= 0 and stop >= 0:
