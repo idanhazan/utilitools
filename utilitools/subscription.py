3be0c24529af9f916fe5571ce2ac589d58cefb1c
idan <idan.hazan.88@gmail.com>
2022-07-15 14:51:07 +0300

    Update subscription.py

--- utilitools/subscription.py
+++ utilitools/subscription.py
@@ -32,25 +32,34 @@ class Subscription:
 
     @staticmethod
     def numeric_slice(key):
-        if key.step is None or key.step > 0:
+        ltr = slice(0, sys.maxsize, 1)
+        rtl = slice(sys.maxsize, -sys.maxsize, -1)
+        if key.step < 0:
             return slice(
-                0 if key.start is None else key.start,
-                sys.maxsize if key.stop is None else key.stop,
-                1 if key.step is None else key.step,
-            )
-        elif key.step < 0:
-            return slice(
-                sys.maxsize if key.start is None else key.start,
-                -sys.maxsize if key.stop is None else key.stop,
-                -1 if key.step is None else key.step,
+                rtl.start if key.start is None else key.start,
+                rtl.stop if key.stop is None else key.stop,
+                rtl.step if key.step is None else key.step,
             )
         else:
-            return slice(0, 0, 0)
+            return slice(
+                ltr.start if key.start is None else key.start,
+                ltr.stop if key.stop is None else key.stop,
+                ltr.step if key.step is None else key.step,
+            )
 
 
 def subscription(iter_type=None, /):
     """
     | A decorator that transforms a function into a subscription object.
+
+    .. note::
+        | A subscription object (in Python) means it implements the ``__getitem__(self, key)`` magic method.
+        | test1: :obj:`__getitem__`
+        | test2: :object:`__getitem__`
+        | test3: :func:`__getitem__`
+
+    | Subscription is a private case of function use (valid for a situation where the function behaves like a sequence).
+    | When needed, the code will be more readable and will also make it easier to write in a "pythonic" style.
 
     :param iter_type:
         | The returned data type when `key` is :func:`slice`.
