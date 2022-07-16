60ec735e1af3feac786a8a06518542838c231e19
idan <idan.hazan.88@gmail.com>
2022-07-16 12:51:09 +0300

    Update singleton.py

--- utilitools/singleton.py
+++ utilitools/singleton.py
@@ -1,10 +1,28 @@
 class singleton(type):
     """
-    A metaclass of Singleton pattern.
+    | A metaclass of Singleton pattern.
+
+    | Singleton pattern is a software design pattern
+      that restricts the instantiation of a class to one “single” instance.
+
+    .. note::
+        | A singleton pattern is “acceptable” when it doesn’t affect the execution of the code.
     """
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
+        """
+        | Verifying a single instance.
+
+        :param args:
+            | Arguments will be passed to the original class constructor.
+        :type args: `any`
+        :param kwargs:
+            | Keyword arguments will be passed to the original class constructor.
+        :type kwargs: `any`
+        :return: An instance of a selected class as a singleton object.
+        :rtype: `any`
+        """
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(singleton, cls).__call__(*args, **kwargs)
         return cls._instances.get(cls)
