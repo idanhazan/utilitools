6aea6a6a0c59767285def1bdefa500e6f83f99d6
idan <idan.hazan.88@gmail.com>
2022-07-29 23:44:59 +0300

    Update flatten.py

--- utilitools/utilities/flatten.py
+++ utilitools/utilities/flatten.py
@@ -5,27 +5,28 @@ def flatten(iterable, /, *, dictionaries='keys', levels=None, types=None):
     """
     | Flatten an iterable with multiple levels of nesting.
 
-    Parameters
-    ----------
-
-    iterable : collections.Iterable
+    :param iterable:
         | Iterable to be flattened.
-    dictionaries : str
+    :type iterable: collections.Iterable
+
+    :param dictionaries:
         | The way iterates over dictionaries.
         | The possible options are `items`, `keys`, and `values`.
         | By default, it will iterate on 'keys'.
-    levels : int
+    :type dictionaries: str
+
+    :param levels:
         | Nesting levels we want to flatten in the iterator.
         | By default, will be set to `flaot('inf')`.
-    types : tuple[type]
+    :type levels: int
+
+    :param types:
         | Base types that we will not refer to in flattening.
         | By default, both `bytes` and `str` are predefined.
+    :type types: tuple[type]
 
-    Yields
-    ------
-
-    typing.Any
-        | An iterator with flatten items.
+    :return: An iterator with flatten items.
+    :rtype: `collections.Iterator[typing.Any]`
     """
     levels = float('inf') if levels is None else levels
     types = (bytes, str) if types is None else (bytes, str) + types
