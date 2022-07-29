d849f6575b097c66464e41a0963c0b0ec8fc50d1
idan <idan.hazan.88@gmail.com>
2022-07-30 00:10:46 +0300

    Update partial.py

--- utilitools/utilities/partial.py
+++ utilitools/utilities/partial.py
@@ -13,25 +13,24 @@ class partial(functools.partial):
           a simplified signature by “freezing” some portion of
           a function’s positional and keyword arguments.
 
-        Parameters
-        ----------
-        function : collections.Callable
+        :param function:
             | Callable to which the partial object will be applied.
-        args : typing.Any
+        :type function: collections.Callable
+
+        :param args:
             | Variadic positional arguments will be passed directly
               to the function during initialization.
-        kwargs : typing.Any
+        :type args: typing.Any
+
+        :param kwargs:
             | Variadic keywords arguments will be passed directly
               to the function during initialization.
+        :type kwargs: typing.Any
 
-        Returns
-        -------
-        utilitools.partial
-            | A partial object with a simplified signature.
+        :return: A partial object with a simplified signature.
+        :rtype: :obj:'utilitools.partial'
 
-        Raises
-        ------
-        TypeError
+        :raises TypeError:
             - If `function` is not callable.
         """
         return super(partial, cls).__new__(cls, function, *args, **kwargs)
@@ -41,18 +40,20 @@ class partial(functools.partial):
         | An extension that accepts `Ellipsis`
           as a placeholder for positional arguments.
 
-        Parameters
-        ----------
-        args : typing.Any
+        :param args:
             | Variadic positional arguments will be passed directly
               to the function after initialization.
-        kwargs : typing.Any
+        :type args: typing.Any
+
+        :param kwargs:
             | Variadic keywords arguments will be passed directly
               to the function after initialization.
+        :type kwargs: typing.Any
 
-        Raises
-        ------
-        BaseException
+        :return: The output of the function.
+        :rtype: typing.Any
+
+        :raises BaseException:
             - If `args` does not match to function.
             - If `kwargs` does not match to function.
         """
