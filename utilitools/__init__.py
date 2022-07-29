1b64382f4f296416f59971e2e1d20b72b4e38dfd
idan <idan.hazan.88@gmail.com>
2022-07-29 23:19:23 +0300

    Update __init__.py

--- utilitools/__init__.py
+++ utilitools/__init__.py
@@ -1,6 +1,7 @@
-from .islice import islice
-from .partial import partial
-from .singleton import singleton
-from .subscription import subscription
+from utilitools.utilities.flatten import flatten
+from utilitools.utilities.islice import islice
+from utilitools.utilities.partial import partial
+from utilitools.utilities.singleton import singleton
+from utilitools.utilities.subscription import subscription
 
 __version__ = (0, 0, 13)
